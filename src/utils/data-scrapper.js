d8bd8e0a7fa4fa73eced1e9d8c02dd259d14c2de
noamMuallem <noammu072@gmail.com>
2020-12-08 12:11:33 +0200

    updated url component in puppeteer script to match new website update

--- src/utils/data-scrapper.js
+++ src/utils/data-scrapper.js
@@ -26,7 +26,7 @@ const scrape = async (courseNumber) => {
   //waite for the page to load is does not load reaturn empty lectures
   await page
     .waitForXPath(
-      "/html/body/div[3]/div[3]/div/div/form/table/tbody/tr[2]/td/div[2]",
+      "/html/body/main/div[4]/div[2]/div/div/form/table/tbody/tr[1]/td/h1",
       { timeout: 8000 }
     )
     .then(async () => {
